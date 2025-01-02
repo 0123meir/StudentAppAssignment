7b9c910bfa3970fb306dddc3ffdcbc4f8b84736f
Meir <0123meir@gmail.com>
2025-01-02 16:39:00 +0200

    WIP: add list of student

---
+++ app/src/main/java/com/example/studentsappassignment/Student.kt
@@ -0,0 +1,9 @@
+package com.example.studentsappassignment
+
+data class Student(
+    val id: Int,
+    val name: String,
+    val phone: String,
+    val address: String,
+    val picture: Int // Assuming you're using drawable resources for pictures
+)
