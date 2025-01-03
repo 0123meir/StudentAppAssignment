68456f7320e4dbf1ba419a4df41a234deee502f4
Meir <0123meir@gmail.com>
2025-01-03 18:00:29 +0200

    added list activity

--- app/src/main/java/com/example/studentsappassignment/MainActivity.kt
+++ app/src/main/java/com/example/studentsappassignment/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.example.studentsappassignment
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.student_item)
 
+    private val mockStudents = listOf(
+        Student(1, "Alice", "123-456-7890", "123 Main St", R.drawable.default_image),
+        Student(2, "Bob", "987-654-3210", "456 Oak Ave", R.drawable.ic_launcher_foreground),
+        Student(3, "Charlie", "555-123-4567", "789 Elm St", R.drawable.default_image)
+    )
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
+
+        val studentsRecyclerView = findViewById<RecyclerView>(R.id.studentsRecyclerView)
+        studentsRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        studentsRecyclerView.adapter = StudentAdapter(mockStudents)
+
+    }
 }
